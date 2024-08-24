e69fbc40259b68dcd421350c7b8076c3f53b22cb
Wasifkhaan <125725658+Wasifkhaan@users.noreply.github.com>
2024-08-25 03:49:01 +0500

    Add files via upload

---
+++ ASSIGNMENT 1 AND 2/index.js
@@ -0,0 +1,98 @@
+//CHP1  
+
+//alert( "hello world" );
+//Swal.fire({
+//    title: "The Internet?",
+//    text: "That thing is still around?",
+//    icon: "question"
+//  });
+
+//CHP 2
+
+// var cars = "alto"
+// alert(cars)
+// cars = "mehran"
+// alert(cars)
+
+// var user_name;
+// user_name = "wasif"
+// alert(user_name)
+
+// ASSIGNMENT NO: 01
+
+// Question 1
+
+// Swal.fire({
+//     title: "hello , sir",
+//     width: 600,
+//     padding: "3em",
+//     color: "#716add",
+//     background: "#fff url(/images/trees.png)",
+//     backdrop: `
+//       rgba(0,0,123,0.4)
+//       url("/images/nyan-cat.gif")
+//       left top
+//       no-repeat
+//     `
+//   });
+
+//Question no: 02
+
+// alert("Error! Please a enter a valid password")
+
+//Question no: 03
+
+// alert("Welcome to Js Land..\nHappy Coding!")
+
+//Question No: 04
+// alert("Welcome to Js Land..")
+// alert("Happy Coding!")
+
+//question no:05
+
+// alert("Hello..i can run JS through my web browser's console")
+
+//ASSIGNMENT NO: 02
+
+// QUESTION NO:01
+
+// var username
+
+//question no:02
+
+// var myName = "WASIF"
+// alert(myName)
+ 
+//question no:03
+
+// var message = "HELLO WORLD!!!"
+// alert(message)
+
+//question no:04
+
+//  var student =("15 years old \n web developer")
+//  alert(student)
+
+//question no: 05
+
+// var pizza =("PIZZA \nPIZZ \nPIZ \nPI \nP")
+// alert(pizza)
+
+//question no:06
+
+// var email =("My email address is wasiffkhann16@gmail.com")
+// alert(email)
+
+//question no: 07
+
+// var book =" I am trying to learn from the book A smarter way to learn JavaScript"
+// alert(book)
+
+//question no: 08
+// var script = ("Yah! i can write HTML content through  Javascript")
+// alert(script)
+
+//question no:09
+
+var design =("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+alert(design)
