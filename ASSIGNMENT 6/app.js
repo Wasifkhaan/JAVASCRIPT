9ef922ddaedb20bd8277c7570131ef3d1ebce492
Wasifkhaan <125725658+Wasifkhaan@users.noreply.github.com>
2024-09-20 01:13:37 +0500

    Add files via upload

---
+++ ASSIGNMENT 6/app.js
@@ -0,0 +1,73 @@
+// var arr = ["Wasif","khan"]
+// console.log(arr.join().split())
+
+
+
+// var cars = ["Alto","Bmw","civic","daala","crown"] 
+// var userCar = prompt("Enter your car name")
+// var Flag = false
+
+// for(i=0;i<cars.length;i++){
+//     if(cars[1]===userCar){
+
+//     }
+// }
+
+
+                                                         //ASSIGNMENT No: 06 (CHP 17-20)
+             //  Question No: 01
+
+// var arr = [[],[],[],[]]
+ 
+             //  Question No: 02
+
+// var arr = [[0, 1, 2, 3],[1, 0, 1, 2], [2, 1, 0, 1] ]
+// document.write(arr[0].join(),"<br/>",arr[1].join(),"<br/>",arr[2].join(),"<br/>")
+
+             // Quetion No: 03
+    
+// for(i=0;i<=10;i++){
+//     document.write(i,"<br/>")
+// }
+ 
+              // Quetion No: 04
+
+// let tab = prompt("enter your num")
+// let tabLength = prompt("Enter Length of Multiplication table")
+// document.write(" multiplication table of " +  tab  + " is:" + "<br/>") 
+// document.write("Length","=" +  tabLength +"<br/>") 
+
+//  for(let i = 1; i<=tabLength; i++){
+//      var res = tab*i
+    
+//     document.write(tab, " x " ,i, " = " ,res + "<br/>"); }
+
+   
+              // Quetion No: 05
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+// for(i=0; i<fruits.length; i++){
+//     document.write(fruits[i] + "<br/>")
+// }
+// for(i=0; i<fruits.length; i++){
+//     document.write("Element of index ",i," is ", fruits[i] + "<br/>")
+// }
+
+                 // Quetion No: 06
+
+// document.write("<h1>Counting</h1>")
+// var counting = prompt("Enter a number")                
+// for(i=0;i<=counting;i++){
+//     document.write(i+",")
+// }
+
+// document.write("<h1>Reverse counting</h1>")
+// var reverseCounting = prompt("Enter a number for reverse counting")                
+// for(i=0; i>=reverseCounting; i--){
+//     document.write(reverseCounting.push(i))
+// }
+
+// document.write("<h1>Even</h1>")
+//  var counting = prompt("Enter a number for Even")                
+//  for(i=0;i<=counting;i++){
+//      document.write(i+",")
+//  }
