cea581b77f417a1f79733f326da906e859a9ac6a
Wasifkhaan <125725658+Wasifkhaan@users.noreply.github.com>
2024-09-27 01:41:57 +0500

    Add files via upload

---
+++ ASSIGNMENT 7/app.js
@@ -0,0 +1,25 @@
+//                         WITH METHOD
+
+// var palindrome = prompt("Enter a word to check palindrome"); 
+// var reversed = palindrome.split('').reverse().join('');
+//         if ((reversed==palindrome)) {
+//             console.log(palindrome, "is a palindrome.");
+//         } else {
+//             console.log(palindrome, "is not a palindrome.");
+//         }
+
+
+
+//                         WITHOUT METHOD 
+
+// var palindrome= prompt("Enter a word to check palindrome")
+// var ispalindrome= " "
+
+// for(var i=palindrome.length-1; i>=0; i--){
+//     ispalindrome = ispalindrome + palindrome[i]
+// }
+// if( palindrome === ispalindrome){
+//     console.log(palindrome,"is a palindrome")
+// }else{
+//     console.log(palindrome,"is not a palindrome")
+// }
