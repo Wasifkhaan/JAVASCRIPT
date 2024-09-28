96930d6b12e59b59e159c79cf3a5a0ae816ff00f
Wasifkhaan <125725658+Wasifkhaan@users.noreply.github.com>
2024-09-28 16:22:10 +0500

    Add files via upload

---
+++ ASSIGNMENT 8/app.js
@@ -0,0 +1,27 @@
+// let numbers = [34, 12, 24, 9, 5, 62, 2];
+// let ascendingNumbers = [numbers]; 
+// let n = ascendingNumbers.length;
+
+// for (let i = 0; i < n - 1; i++) {
+//     for (let j = 0; j < n - i - 1; j++) {
+//         if (ascendingNumbers[j] > ascendingNumbers[j + 1]) {
+//             let temp = ascendingNumbers[j];
+//             ascendingNumbers[j] = ascendingNumbers[j + 1];
+//             ascendingNumbers[j + 1] = temp;
+//         }
+//     }
+// }
+// console.log("Sorted in Ascending Order:", ascendingNumbers);
+
+
+// let descendingNumbers = [numbers]; 
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n - 1; j++) {
+//         if (descendingNumbers[j] < descendingNumbers[j + 1]) {
+//             let temp = descendingNumbers[j];
+//             descendingNumbers[j] = descendingNumbers[j + 1];
+//             descendingNumbers[j + 1] = temp;
+//         }
+//     }
+// }
+// console.log("Sorted in Descending Order:", descendingNumbers);
