9060842744f0254fa7c095ca976a4d26541521b7
Muhammad Wasif <125725658+Wasifkhaan@users.noreply.github.com>
2024-09-29 02:08:17 +0500

    Add files via upload

---
+++ ASSIGNMENT 8/app.js
@@ -0,0 +1,24 @@
+
+//                                   Ascending order
+
+// var number = [8, 2, 4, 6, 10, 14, 12];
+// for (var i = 0; i < number.length - 1; i++) {
+//     for (var j = 0; j < number.length - 1 - i; j++) {
+//         if (number[j] > number[j + 1]) {
+//             [number[j], number[j + 1]] = [number[j + 1], number[j]];
+//         }
+//     }
+// }
+// document.write("Ascending Order: " + number + "<br>"); 
+
+//                                    Descending order
+// var number1 = [1, 13, 3, 7, 9, 5, 11];
+// for (var i = 0; i < number1.length - 1; i++) {
+//     for (var j = 0; j < number1.length - 1 - i; j++) {
+//         if (number1[j] < number1[j + 1]) {
+//             [number1[j], number1[j + 1]] = [number1[j + 1], number1[j]];
+//         }
+//     }
+// }
+// document.write("Descending Order: " + number1); 
+
