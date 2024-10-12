900343e87384c02f905e843009c74445e5ede00d
Muhammad Wasif <125725658+Wasifkhaan@users.noreply.github.com>
2024-10-13 02:19:12 +0500

    Add files via upload

---
+++ ASSIGNMENT 13/app.js
@@ -0,0 +1,48 @@
+
+
+
+//                                                              ASSIGNMENT NO:13
+
+
+
+//                                                               QUESTION NO:01
+
+
+var Day = new Date();
+console.log(Day);
+
+//                                                               Question No: 02
+
+
+// var currentMonth = ["january","February","March","April","May","June","july","august","September","october","November","December"]
+// alert("Current Month: " + currentMonth[Day.getMonth()])
+
+//                                                                Question No: 03
+
+
+// var currentDay = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+// alert("Current Day: " + currentDay[Day.getDay()].slice(0,3)) 
+
+
+//                                                                  Question No: 04
+
+
+// var Days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+// if(Days[Day.getDay]==="Saturday" || Days[Day.getDay]==="Sunday"){
+//     alert("Its Fun Day :)")
+
+// }
+// else{
+//     alert("kaam kr kaaam")
+// }
+
+//                                                                   QUESTION NO:05
+
+// if(Day.getDay() <= 15){
+//     alert("First fifteen days of the month:")
+// } 
+// else{
+//     alert("nh ptaa")
+// }
+
+//                                                                   Question No: 06
